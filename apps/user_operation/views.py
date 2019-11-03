46232622052042c8e69be081d384b6086954ad5d
suyongzhi <1125699801@qq.com>
2019-11-03 17:39:10 +0800

    增加用户收藏功能(收藏功能暂存在BUG，待修复)

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 
 from .models import UserFav
 from .serializers import UserFavSerializer
+from utils.permissions import IsOwnerOrReadOnly
 
 
-class UserFavViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.DestroyModelMixin,mixins.ListModelMixin):
+class UserFavViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin):
     """
     用户收藏功能
     """
-    queryset = UserFav.objects.all()
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    lookup_field = "goods_id"
+
+    def get_queryset(self):
+        return UserFav.objects.filter(user=self.request.user)
