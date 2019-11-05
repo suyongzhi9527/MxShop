5c12f6f0ac4f347c4c9fd87e9abe3b18ff7776e1
suyongzhi <1125699801@qq.com>
2019-11-05 11:31:14 +0800

    增加用户中心留言功能

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -3,19 +3,49 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication
 
-from .models import UserFav
-from .serializers import UserFavSerializer
+from .models import UserFav, UserLeavingMessage
+from .serializers import UserFavSerializer, UserFavDetailSerializer, LeavingMessageSerializer
 from utils.permissions import IsOwnerOrReadOnly
 
 
 class UserFavViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin):
     """
-    用户收藏功能
+    list:
+        获取用户收藏列表
+    retrieve:
+        判断某个商品是否已经收藏
+    create:
+        收藏商品
     """
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
-    serializer_class = UserFavSerializer
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
     lookup_field = "goods_id"
 
     def get_queryset(self):
         return UserFav.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserFavDetailSerializer
+        elif self.action == "create":
+            return UserFavSerializer
+        return UserFavSerializer
+
+
+class LeavingMessageViewset(mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.CreateModelMixin,
+                            viewsets.GenericViewSet):
+    """
+    list:
+        获取用户留言
+    create:
+        添加留言
+    delete:
+        删除留言
+    """
+
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    serializer_class = LeavingMessageSerializer
+
+    def get_queryset(self):
+        return UserLeavingMessage.objects.filter(user=self.request.user)
